edb7470e1094ee84f2e074ec46f1e20f6d95d4db
raster-Q <119475152+raster-Q@users.noreply.github.com>
2022-12-23 10:46:52 +0900

    フォトビューワー始動

--- src/photo01.js
+++ src/photo01.js
@@ -1,3 +1,9 @@
 import "./styles.css";
 
-
+class photoviewer {
+  init() {
+    const view = document.getElementById("view");
+    const prev = document.querySelector(".prev");
+    const next = document.querySelector(".address");
+  }
+}
